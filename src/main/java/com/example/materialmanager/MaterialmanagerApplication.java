a99e7c21568bbf51c0f25fcb25ff30151da16e78
Krystal <motionbit.dev@gmail.com>
2025-08-29 09:51:03 +0900

    spring initializr로 의존성 포함하여 generate

---
+++ src/main/java/com/example/materialmanager/MaterialmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.materialmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/materialmanager/MaterialmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.materialmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
